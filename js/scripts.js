eaf5c5fecd1fcf055eae64a72c3f1e540a6a2f2a
brendan grubb <brendangrubb@gmail.com>
2017-01-13 16:01:45 -0800

    add functional script to form, add some styling, add Take Quiz Again button

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,43 @@
 $(document).ready(function() {
 
+  $("#survey-questions").submit(function(event){
+    event.preventDefault();
+
+    var question1Input = $("#question1").val();
+    var question2Input = $("#question2").val();
+    var question3Input = $("#question3").val();
+    var question4Input = $("#question4").val();
+    var question5Input = $("#question5").val();
+
+
+
+    if (question1Input === "question1Design") {
+      $("#intro-text").slideUp();
+      $("#survey-questions").slideUp();
+      $("#suggestion1").fadeIn(1000);
+      $(".quiz-again").fadeIn(4000);
+    } else if (question2Input === "question2Java") {
+      $("#intro-text").slideUp();
+      $("#survey-questions").slideUp();
+      $("#suggestion2").fadeIn(1000);
+      $(".quiz-again").fadeIn(4000);
+    } else if (question3Input === "question3C-sharp") {
+      $("#intro-text").slideUp();
+      $("#survey-questions").slideUp();
+      $("#suggestion3").fadeIn(1000);
+      $(".quiz-again").fadeIn(4000);
+    } else if (question4Input === "question4C-sharp") {
+      $("#intro-text").slideUp();
+      $("#survey-questions").slideUp();
+      $("#suggestion3").fadeIn(1000);
+      $(".quiz-again").fadeIn(4000);
+    } else if (question5Input === "question5Design")
+    $("#intro-text").slideUp();
+    $("#survey-questions").slideUp();
+    $("#suggestion1").fadeIn(1000);
+    $(".quiz-again").fadeIn(4000);
+
+  })
 
 
 
@@ -12,5 +50,4 @@ $(document).ready(function() {
 
 
 
-  
 })
